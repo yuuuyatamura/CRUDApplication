513fb7a84ba78b09e7c9d59febec0c8544873962
yuytamura <yt0829xwp@gmail.com>
2020-04-29 21:06:39 +0900

    GitHubにpush

---
+++ src/main/java/com/example/demo/trySpring/JDBCTemplete.java
@@ -0,0 +1,12 @@
+package com.example.demo.trySpring;
+
+import java.util.Map;
+
+public class JDBCTemplete {
+
+	public Map<String, Object> queryForMap(String query, int id) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
